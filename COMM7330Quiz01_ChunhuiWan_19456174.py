037173fa5d8a333ef198c9a61bdd2d37a45309a0
awan37 <55082804+awan37@users.noreply.github.com>
2019-09-14 21:55:55 +0800

    Add files via upload

---
+++ COMM7330Quiz01_ChunhuiWan_19456174.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!I am WANChunhui19456174")
+
+
+# In[2]:
+
+
+print("Hello World!I am ChunhuiWan19456174")
+
+
+# In[ ]:
+
+
+
+
